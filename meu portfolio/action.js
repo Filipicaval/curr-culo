a37a43b72c778d5feea2b994e312001e8b033f9a
Filipi Cavalcante <filipicavalcante30@gmail.com>
2025-08-24 03:14:41 +0100

    Delete meu portfolio directory

--- meu portfolio/action.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById("form-contato");
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  document.getElementById("mensagem-sucesso").style.display = "block";
-});
